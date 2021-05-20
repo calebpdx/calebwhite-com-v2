ffa2d7c1c98b18b7c0cbb305a3d8746a36557717
Caleb White <caleb@fairwaycreative.com>
2021-05-20 14:05:48 -0700

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { config } from '@fortawesome/fontawesome-svg-core'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import normalize from 'normalize.css/normalize.css'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    background-color: #020202;
+  	font-family: Arial, Helvetica, sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -9,18 +17,28 @@ const GlobalStyle = createGlobalStyle`
 `
 
 const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+	colors: {
+		powderWhite: '#FFFDF9',
+		persianGreen: '#06B49A',
+		lightBlue: '#AFDBD2',
+		onyx: '#36313D',
+	},
+	fonts: ['Helvetica', 'sans-serif', 'Roboto'],
+	fontSizes: {
+		xs: '0.75em',
+		sm: '1em',
+		md: '2em',
+		lg: '3em',
+	},
 }
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+	return (
+		<>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	)
 }
