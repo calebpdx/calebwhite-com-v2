a02a8d736ffc6d84bfe5979ebabe78dd24392430
Caleb White <caleb@fairwaycreative.com>
2021-05-25 14:39:05 -0700

    added svg and svg styling to the logo

---
+++ components/logo.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from '../themes/_darkTheme'
+
+const SvgStyle = styled.svg`
+	height: 60px;
+	width: 50px;
+	fill: ${({ theme }) => theme.colors.powderWhite};
+
+	#w-letter {
+		fill: ${({ theme }) => theme.colors.secondary};
+	}
+
+	:hover {
+		width: 60px;
+		fill: ${({ theme }) => theme.colors.primary};
+		transition: width 0.25s ease-in-out;
+		transition: fill 0.25s ease-in-out;
+
+		#w-letter {
+			fill: ${({ theme }) => theme.colors.powderWhite};
+			transition: fill 0.25s ease-in;
+		}
+	}
+`
+
+const Logo = () => {
+	return (
+		<SvgStyle
+			version='1.1'
+			xmlns='http://www.w3.org/2000/svg'
+			x='0px'
+			y='0px'
+			width='432px'
+			height='577.4px'
+			viewBox='0 0 432 577.4'
+		>
+			<g id='c-letter'>
+				<rect id='c-top' width='432' height='107.8' />
+				<rect
+					id='c-left'
+					x='-234'
+					y='234'
+					transform='matrix(6.123234e-17 -1 1 6.123234e-17 -234 342)'
+					width='576'
+					height='108'
+				/>
+				<rect id='c-bottom' y='468' width='432' height='109.4' />
+			</g>
+			<g id='w-letter'>
+				<rect
+					id='w-left_1_'
+					x='144'
+					y='144'
+					className='st0'
+					width='72'
+					height='288'
+				/>
+				<rect
+					id='w-middle_1_'
+					x='252'
+					y='216'
+					className='st0'
+					width='72'
+					height='216'
+				/>
+				<rect
+					id='w-right_1_'
+					x='360'
+					y='144'
+					className='st0'
+					width='72'
+					height='288'
+				/>
+
+				<rect
+					id='w-bottom_1_'
+					x='288'
+					y='288'
+					transform='matrix(-1.836970e-16 1 -1 -1.836970e-16 720 72)'
+					className='st0'
+					width='72'
+					height='216'
+				/>
+			</g>
+		</SvgStyle>
+	)
+}
+
+export default Logo
