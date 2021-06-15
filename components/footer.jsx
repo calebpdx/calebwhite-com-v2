9fff2b9ac0f88f4ee9feac535311b8d31719a55e
Caleb White <caleb@fairwaycreative.com>
2021-06-15 16:59:58 -0700

    added functionality to fetch github repo status

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,45 +1,119 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'next/link'
 import Moment from 'react-moment'
+import useSWR from 'swr'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faStar, faCodeBranch, faCode } from '@fortawesome/free-solid-svg-icons'
 
 import { respondAt } from '../utils/_respondAt'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+function getRepo() {
+	const { data, error } = useSWR(
+		'https://api.github.com/repos/calebpdx/calebpdx',
+		fetcher
+	)
+
+	if (error) return <div>failed to load</div>
+	if (!data) return <div>loading...</div>
+	return data
+}
 
 const Container = styled.footer`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
 	align-items: center;
-	width: 100%;
-	min-height: 2.5rem;
-	padding: 2rem 0;
+	width: 80%;
+	padding: 1rem 0;
 	background-color: ${({ theme }) => theme.colors.bg.main};
 	color: ${({ theme }) => theme.colors.altLight};
 	font-size: 1rem;
 
-	${respondAt.sm`
+	${respondAt.lg`
 		flex-direction: row;
 		width: 70%;
 		max-width: 1520px;
 	`}
+
+	a {
+		font-size: ${({ theme }) => theme.fontSizes.sm};
+		line-height: 1;
+		font-weight: 400;
+	}
 `
 
 const Copyright = styled.div`
+	margin: auto;
 	display: flex;
-	align-items: flex-start;
+	text-align: center;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+
+	${respondAt.lg`
+		justify-content: flex-start;
+	`}
 `
-const Source = styled.div`
+const Source = styled.button`
+	color: ${({ theme }) => theme.colors.altLight};
+	background-color: ${({ theme }) => theme.colors.buttons.primary};
+	border: 0.15em solid ${({ theme }) => theme.colors.buttons.secondary};
+	border-radius: 0.35em;
 	display: flex;
+	flex-direction: row;
+	min-width: 10rem;
+	align-items: center;
 	justify-items: flex-end;
+	align-content: center;
+	justify-content: center;
+	padding: 0.25em;
+
+	div {
+		padding: 0.25rem;
+	}
+
+	h4 {
+		margin: 0;
+	}
+
+	.gh {
+		padding: 0 0.1rem 0 0.25rem;
+	}
 `
 
 const Footer = () => {
+	const data = getRepo()
+
 	return (
 		<Container>
 			<Copyright>
-				Copyright &copy; <Moment format='YYYY' /> &nbsp; Caleb White.
-				All Rights Reserved.
+				<p>
+					Copyright &copy; <Moment format='YYYY' /> Caleb White. All
+					Rights Reserved.{' '}
+					<Link href='/siteinfo'>
+						<a>Site Information</a>
+					</Link>
+				</p>
 			</Copyright>
-			<Source>Source GITHUB</Source>
+			<Source>
+				<div>source</div>
+				<div>
+					<FontAwesomeIcon icon={faStar} size='sm' className='gh' />{' '}
+					{data.stargazers_count}
+				</div>
+				<div>
+					<FontAwesomeIcon
+						icon={faCodeBranch}
+						size='sm'
+						className='gh'
+					/>{' '}
+					{data.forks_count}
+				</div>
+			</Source>
 		</Container>
 	)
 }
