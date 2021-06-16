90ee3b94633bfe7a8fede44d2ff7d1967edc099c
Caleb White <caleb@fairwaycreative.com>
2021-06-16 15:22:58 -0700

    init privacy page

---
+++ pages/privacy.js
@@ -0,0 +1,60 @@
+/* eslint-disable react/no-unescaped-entities */
+
+// Dependencies
+import React from 'react'
+import Head from 'next/head'
+import styled, { ThemeProvider } from 'styled-components'
+import Headroom from 'react-headroom'
+
+// Required Components
+
+import PageWrapper from '../components/_helpers/pageWrapper'
+
+import Footer from '../components/footer'
+
+import Navigation from '../components/navigation'
+
+// Utilities Used
+// import { respondAt } from '../utils/_respondAt'
+
+// Theming Components/Utils
+import { useDarkMode } from '../themes/useDarkMode'
+import { mainTheme, darkTheme } from '../themes/themes'
+import GlobalStyles from '../themes/globalStyles'
+
+const Container = styled.section`
+	width: 100%;
+`
+
+export default function Home() {
+	const [theme, toggleTheme] = useDarkMode()
+	const themeMode = theme === 'mainTheme' ? mainTheme : darkTheme
+
+	return (
+		<>
+			<Head>
+				<title>Caleb White - Frontend Engineer - Portland, OR</title>
+				<meta
+					name='Caleb White'
+					content='A Frontend Engineer from Portland, OR'
+				/>
+
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<ThemeProvider theme={themeMode}>
+				<GlobalStyles />
+
+				<PageWrapper>
+					<Headroom style={{ zIndex: 998 }}>
+						<Navigation theme={theme} toggleTheme={toggleTheme} />
+					</Headroom>
+					<Container>
+						<h2>Site Information</h2>
+					</Container>
+
+					<Footer />
+				</PageWrapper>
+			</ThemeProvider>
+		</>
+	)
+}
