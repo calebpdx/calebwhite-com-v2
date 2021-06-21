d319c461fdf27cbf1a241a1b3520fdc228a3006f
Caleb White <caleb@fairwaycreative.com>
2021-06-20 19:07:48 -0700

    Added new styles and mapped the ProjectCard componente

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import ProjectCard from '@components/projects/projectCard'
 
 import { respondAt } from '../utils/_respondAt'
 
-const Wrapper = styled.div`
-	background-color: ${({ theme }) => theme.colors.bg.secondary};
+const client = require('contentful').createClient({
+	space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+	accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+})
+
+const Wrapper = styled.section`
+	background-color: ${({ theme }) => theme.colors.bg.main};
+	color: ${({ theme }) => theme.colors.text.primary};
 	width: 100%;
 	display: flex;
 	justify-content: center;
+	border-top: 0.15em solid ${({ theme }) => theme.colors.bg.main};
+	border-bottom: 0.15em solid ${({ theme }) => theme.colors.bg.card};
 	transition: all 0.25s linear;
 `
 
@@ -15,12 +24,12 @@ const Container = styled.section`
 	display: flex;
 	flex-direction: column;
 	flex-wrap: wrap;
-	align-content: center;
 	justify-content: center;
+	align-items: center;
 	width: 80%;
 	margin: 3rem 2rem;
 
-	${respondAt.sm`
+	${respondAt.md`
     	flex-direction: row;
 		width: 70%;
         max-width: 1520px;
@@ -28,53 +37,67 @@ const Container = styled.section`
 `
 
 const Title = styled.h2`
-	order: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	width: 100%;
 	flex-basis: 0 1;
 	font-weight: 600;
+	margin-bottom: 0.5em;
 `
-const Work = styled.section`
+
+const ProjectList = styled.div`
 	display: flex;
-	flex-direction: row;
-	order: 2;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 	width: 100%;
-`
-
-const SortJobs = styled.aside`
-	order: 1;
-	width: 100%;
-	margin-top: 1.5rem;
 
 	${respondAt.sm`
-        order: none;
-
-	    width: 30%;
-	`}
-`
-
-const JobDescriptions = styled.div`
-	order: 2;
-	width: 100%;
-	margin-top: 1.5rem;
-
-	${respondAt.sm`
-        order: 2;
-	    padding: 0 3rem 0 0;
-	    width: calc(70% - 3rem);
-	`}
+		justify-content: flex-start;
+		align-items: flex-start;
+    	flex-direction: row;
+		width: 100%;
+        max-width: 1520px;
+	`};
 `
 
 const Projects = () => {
+	const [projects, setProjects] = useState([])
+
+	const fetchEntries = async () => {
+		const projects = await client.getEntries({
+			content_type: 'projects',
+		})
+		if (projects.items) return projects.items
+		// eslint-disable-next-line no-undef
+		console.log(`Error getting Entries for ${contentType.name}.`)
+	}
+
+	useEffect(() => {
+		async function getProjects() {
+			const newProjects = await fetchEntries()
+
+			// Sort projects descending
+			const allProjects = []
+				.concat(newProjects)
+				.sort((a, b) => (a.fields.title > b.fields.title ? -1 : 1))
+
+			setProjects([...allProjects])
+		}
+
+		getProjects()
+	}, [])
+
 	return (
 		<Wrapper id='projects'>
 			<Container>
-				<Title>Projects</Title>
-				<Work>
-					<SortJobs>List Jobs</SortJobs>
-					<JobDescriptions>
-						Job descriptions and sorting
-					</JobDescriptions>
-				</Work>
+				<Title>Featured Projects</Title>
+				<ProjectList>
+					{projects.map((project) => (
+						<ProjectCard key={project.sys.id} project={project} />
+					))}
+				</ProjectList>
 			</Container>
 		</Wrapper>
 	)
