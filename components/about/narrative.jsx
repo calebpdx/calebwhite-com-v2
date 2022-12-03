8dc260d202b4754a5b5aa7f8fbd9acb11b12feb8
Caleb White <calebawhite@gmail.com>
2022-12-02 16:08:47 -0800

    updated narrative section

--- components/about/narrative.jsx
+++ components/about/narrative.jsx
@@ -18,26 +18,23 @@ const Narrative = () => {
 	return (
 		<Nar>
 			<p>
-				Hi, I&lsquo;m Caleb White. I am a frontend engineer living in
+				Hi, I&lsquo;m Caleb White. I am a full-stack engineer living in
 				Portland, OR. I enjoy the challenge of building better user
 				digital experiences, balancing simplicity, functionality and
-				accessibility.
+				accessibility on the web.
 			</p>
 			<p>
-				The diversity of my professional experience includes nonprofit
-				program leadership, senior retail leadership, omni-channel
-				retail, and freelancing. I leverage the insights gained from all
+				My experience is multi-faceted, having worked in a variety of roles. Currently I am a full-stack engineer at{' '}<a href="https://deptagency.com" title="Dept Agency">Dept</a> building robust fullstack applications.
+				My professional experience includes nonprofit
+				program leadership, senior omni-channel
+				retail leadership, frontend engineering and freelancing. I leverage the insights gained from all
 				of these roles, which provides me with a unique perspective on
-				challenges facing businesses today. My primary focus is bringing
-				the in-store retail experience online, and propelling businesses
-				to the next level.
+				challenges facing businesses today. 
 			</p>
 			<p>
 				I follow atomic design principles for creating DRY components in
-				my projects. Thus creating lean, reusable, and scalable code. I
-				currently focus on Next.js/React and headless e-commerce.
+				my projects. Thus creating lean, reusable, and scalable code. Currently I am focused on building scalable
 			</p>
-			<p>I am available for full-time and freelance work.</p>
 		</Nar>
 	)
 }
